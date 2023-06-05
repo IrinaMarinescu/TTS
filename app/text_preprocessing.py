6b51e4be95d27c053f8239d07b9e83b8b0cc64cb
IrinaMarinescu <I.I.Marinescu@student.tudelft.nl>
2023-06-05 14:24:01 +0200

    Fixed the identation error:)

--- app/text_preprocessing.py
+++ app/text_preprocessing.py
@@ -1,6 +1,4 @@
 from app.tokenization import Strategy
 
 
-def preprocess(text: str, strategy: Strategy):
-    # split the string into sentences
-    # for each sentence apply pos_tagging(it calls tokenization by default)
+#def preprocess(text: str, strategy: Strategy):
